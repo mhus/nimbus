a68085061de335fd3681bc6c7ad1efba6818d65a
Mike Hummel <mike@mhus.de>
2025-11-18 09:22:51 +0100

    server

--- server/shared/src/main/java/de/mhus/nimbus/shared/user/UniverseRoles.java
+++ server/shared/src/main/java/de/mhus/nimbus/shared/user/UniverseRoles.java
@@ -1,10 +1,10 @@
-package de.mhus.nimbus.shared.security;
+package de.mhus.nimbus.shared.user;
 
 /**
  * Rollen im System. Diese Enum kann bei Bedarf erweitert werden.
  * Die Speicherung in der User-Entität erfolgt als kommaseparierte Liste der Enum-Namen.
  */
-public enum Roles {
+public enum UniverseRoles {
     USER,
     ADMIN;
 }
