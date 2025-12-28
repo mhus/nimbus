20124c2d1afa3c33e58ea6d105705e85dae89f39
Mike Hummel <mike@mhus.de>
2025-12-28 17:43:45 +0100

    deployment

--- server/deployment/docker-native/build-docker.sh
+++ server/deployment/docker-native/build-docker.sh
@@ -6,4 +6,18 @@ cd ../..
 
 # export BP_PLATFORM_VARIANTS="linux/amd64,linux/arm64"
 
-mvn -Pnative spring-boot:build-image
+mvn clean install -DskipTests
+
+cd world-player
+mvn -Pnative spring-boot:build-image -DskipTests
+cd ..
+
+cd world-control
+mvn -Pnative spring-boot:build-image -DskipTests
+cd ..
+
+cd world-life
+mvn -Pnative spring-boot:build-image -DskipTests
+cd ..
+
+
--- server/world-control/build-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# export BP_PLATFORM_VARIANTS="linux/amd64,linux/arm64"
-
-mvn -Pnative spring-boot:build-image
-
